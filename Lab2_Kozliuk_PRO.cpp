b8184549e4cd361c10f9f40a4e5468653cebb99c
Dmytro Kzklk <Dmytro.Kozliuk.KI.2020@edu.lpnu.ua>
2023-03-13 15:51:32 +0200

    Code improvment

--- Lab2_Kozliuk_PRO.cpp
+++ Lab2_Kozliuk_PRO.cpp
@@ -6,12 +6,33 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_vector.h>
 
-char input_validator(const char* message = NULL) 
+
+enum RESULTS
+{
+	R_FILE = 1,
+	R_CONSOLE,
+	R_BOTH,
+	R_NONE,
+	R_INIT
+};
+
+enum FILLING
+{
+	F_RANDOM,
+	F_KEYBOARD,
+	F_EXIT,
+	F_DEFAULT
+};
+
+FILLING input_validator(const char* message = NULL)
 {
 	if (message != NULL)
 		printf(message);
-	static char choice = 0;
-	while (choice != 1 && choice != 2 && choice != 3)
+
+
+	static FILLING choice = F_DEFAULT;
+
+	while (choice != F_RANDOM && choice != F_KEYBOARD && choice != F_EXIT)
 	{
 		printf("Enter '1' to fill randomly"
 			   "\nEnter '2' to fill from keyboard"
@@ -19,8 +40,8 @@ char input_validator(const char* message = NULL)
 			   "\nYour choice is ");
 		scanf_s("%d", &choice);
 	
-		if (choice < 1 || choice > 3) 
-			printf("Incorect input!\n");
+		if (choice != F_RANDOM && choice != F_KEYBOARD && choice != F_EXIT)
+			printf("Incorrect input!\n");
 	}
 
 	return choice;
@@ -45,60 +66,70 @@ void input_matrix(gsl_matrix* matrix)
 		printf("Row %d: ", i + 1);
 		for (int j = 0; j < matrix->size2; j++) 
 		{
-			scanf_s("%lf", gsl_matrix_ptr(matrix, i, j)); // введення елементу матриці
+			scanf_s("%lf", gsl_matrix_ptr(matrix, i, j)); 
 		}
 	}
 } 
-void input_vector(gsl_vector* vector) {
+void input_vector(gsl_vector* vector)
+{
 	
 	for (int j = 0; j < vector->size; j++) 
 	{
 		printf("Row %d: ", j + 1);
-		scanf_s("%lf", gsl_vector_ptr(vector, j)); // введення елементу матриці
+		scanf_s("%lf", gsl_vector_ptr(vector, j)); 
 	}
 }
-void output_matrix(gsl_matrix* matrix) 
+void output_matrix(const gsl_matrix* matrix) 
 {
-	for (int i = 0; i < matrix->size1; i++) {
-		for (int j = 0; j < matrix->size1; j++) {
+	for (int i = 0; i < matrix->size1; i++) 
+	{
+		for (int j = 0; j < matrix->size1; j++) 
+		{
 			printf("%g\t", gsl_matrix_get(matrix, i, j));
 		}
 		printf("\n");
 	}
 }
-void output_vector(gsl_vector* vector) 
+void output_vector(const gsl_vector* vector) 
 {
-	for (int i = 0; i < vector->size; i++) {
+	for (int i = 0; i < vector->size; i++) 
+	{
 		printf("%0.2lf\n", gsl_vector_get(vector, i));
 	}
 }
-void control_matrix_input(gsl_matrix* matrix, char choice) {
-	switch (choice) {
-	case 1:
+void control_matrix_input(gsl_matrix* matrix, const char choice)
+{
+	switch (choice)
+	{
+	case F_RANDOM:
 		generate_random_num_matrix(matrix);
 		output_matrix(matrix);
 		break;
-	case 2:
+	case F_KEYBOARD:
 		input_matrix(matrix);
 		break;
-	case 3:
+	case F_EXIT:
 		exit(1);
-	};
+	default: ;
+	}
 };
-void control_vector_input(gsl_vector* vector, char choice) {
+void control_vector_input(gsl_vector* vector, const char choice)
+{
 	switch (choice) {
-	case 1:
+	case F_RANDOM:
 		generate_random_num_vector(vector);
 		output_vector(vector);
 		break;
-	case 2:
+	case F_KEYBOARD:
 		input_vector(vector);
 		break;
-	case 3:
+	case F_EXIT:
 		exit(1);
-	};
+	default: ;
+	}
 };
-gsl_vector* calculate_y1(gsl_matrix* matrix, gsl_vector* vector, int* matrix_dimension) {
+gsl_vector* calculate_y1( gsl_matrix* matrix, const gsl_vector* vector, const int* matrix_dimension)
+{
 
 	gsl_vector* result = gsl_vector_alloc(*matrix_dimension);
 	double alpha = 1.0;
@@ -106,12 +137,12 @@ gsl_vector* calculate_y1(gsl_matrix* matrix, gsl_vector* vector, int* matrix_dim
 	gsl_blas_dgemv(CblasNoTrans, alpha, matrix, vector, beta, result);
 	return result;
 }
-gsl_vector* calculate_b(int* matrix_dimension) {
-
+gsl_vector* calculate_b(int* matrix_dimension)
+{
 	gsl_vector* v = gsl_vector_alloc(*matrix_dimension);
 	for (int i = 1; i <= v->size; i++) {
 		if (i % 2 == 0) {
-			gsl_vector_set(v, i - 1, pow(i, 1) / 12);
+			gsl_vector_set(v, i - 1, pow(i, 2) / 12);
 		}
 		else {
 			gsl_vector_set(v, i - 1, i);
@@ -120,17 +151,64 @@ gsl_vector* calculate_b(int* matrix_dimension) {
 
 	return v;
 }
+gsl_vector* calculate_y2(int* matrix_dimension, const gsl_vector* b1, const gsl_vector* c1, const gsl_matrix* A1)
+{
+	gsl_vector* result = gsl_vector_alloc(*matrix_dimension); // where n is the size of the vectors
 
-int main() {
+	double alpha = 12.0; // coefficient for b1
+
+	gsl_vector_memcpy(result, b1); // copy b1 to result vector
+
+	gsl_vector_scale(result, alpha); // multiply result by 12
+
+	gsl_vector_sub(result, c1); // subtract c1 from result
+
+	return result;
+}
+
+int get_matrix_dimension()
+{
 	int matrix_dimension = 0;
-	
 	while (matrix_dimension < 1)
 	{
 		printf("Enter the dimension of the square matrix:");
 		scanf_s("%d", &matrix_dimension);
 		if (matrix_dimension < 1)
-			printf("Incorect matrix dimension!\n");
+			printf("Incorrect matrix dimension!\n");
 	}
+
+	return matrix_dimension;
+
+}
+
+RESULTS get_intermediate_result()
+{
+	RESULTS print_intermediate_result = R_INIT;
+
+	printf("**********************************");
+
+	while (print_intermediate_result != R_FILE
+		&& print_intermediate_result != R_CONSOLE
+		&& print_intermediate_result != R_BOTH
+		&& print_intermediate_result != R_NONE)
+	{
+		printf("\nDo you wanna to output intermediate result?\n");
+		printf("Enter '1' to write to file "
+			"\nEnter '2' to write to console "
+			"\nEnter '3' to write to both console and file"
+			"\nEnter '4' to cancel write intermediate results"
+			"\nYour choice is ");
+		scanf_s("%d", &print_intermediate_result);
+
+		if (print_intermediate_result < 1 || print_intermediate_result > 4)
+			printf("Incorrect input!\n");
+	}
+	return print_intermediate_result;
+}
+
+int main() {
+	int matrix_dimension = get_matrix_dimension();
+	RESULTS print_intermediate_result = get_intermediate_result();
 
 	gsl_matrix* A = gsl_matrix_alloc(matrix_dimension, matrix_dimension); 
 	gsl_vector* b = calculate_b(&matrix_dimension);					 
@@ -154,10 +232,22 @@ int main() {
 	printf("Vector b:\n");
 	output_vector(b);
 	printf("**********************************");
-	 y1 = calculate_y1(A, b, &matrix_dimension);
+
+	y1 = calculate_y1(A, b, &matrix_dimension);
 	printf("Formula y1 = A * b\n y1:\n");
 	output_vector(y1);
 	printf("**********************************");
+
+	control_matrix_input(A1, input_validator("\nEnter the values of the matrix A1:\n"));
+	printf("**********************************");
+
+	control_vector_input(b1, input_validator("\nEnter column-vector b1:\n"));
+	printf("**********************************");
+
+	control_vector_input(c1, input_validator("\nEnter column-vector c1:\n"));
+	printf("**********************************");
+
+
 
 	return 0;
 }
